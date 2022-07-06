0ea41752c04278d6396dc1e473ec38ba4617be69
Chandradev <chandradev_ps@dellteam.com>
2022-07-06 19:55:04 +0530

    Add project files.

---
+++ BlazorApp11/Pages/Parent.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using BlazorApp11.Shared;
+
+namespace BlazorApp11.Pages
+{
+    public partial class Parent : ComponentBase
+    {
+        [Inject]
+        IJSRuntime JsRuntime { get; set; }
+
+        string message;
+        ChatSendBox chatSendBox;
+        void OnClick()
+        {
+            JsRuntime.InvokeAsync<object>("alert", new[] { message });
+            message = "";
+            chatSendBox.FocusAsync();
+        }
+    }
+}
---
+++ BlazorApp11/Shared/ChatSendBox.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorApp11.Shared
+{
+    public partial class ChatSendBox : ComponentBase
+    {
+        [Parameter]
+        public string Value { get; set; }
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public string Placeholder { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        [Parameter]
+        public Action OnClick { get; set; }
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
+        public async Task Click()
+        {
+            await ValueChanged.InvokeAsync(Value);
+            OnClick?.Invoke();
+        }
+
+        public ValueTask FocusAsync() => input.FocusAsync();
+
+        public void Disable()
+        {
+            IsDisabled = true;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Enable()
+        {
+            IsDisabled = false;
+            InvokeAsync(StateHasChanged);
+        }
+
+        private ElementReference input;
+    }
+}
